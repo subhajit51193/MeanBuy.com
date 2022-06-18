b059264cd224882415668cc8fc415f76b70bfe78
Mano Kumar <manojyad436@gmail.com>
2022-06-18 15:16:09 +0530

    file added

--- newarrival.js
+++ newarrival.js
@@ -1,132 +1,13 @@
 let newarrival = [
   {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzg2XC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
+    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMyNjMxXC9HcmVlbl9uXzFfMS5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjMyMCwiaGVpZ2h0IjozMjAsImZpdCI6ImNvbnRhaW4iLCJiYWNrZ3JvdW5kIjp7InIiOjI1NSwiZyI6MjU1LCJiIjoyNTUsImFscGhhIjoxfX19fQ==",
+    name: "Mens Spining Wheel Watch",
+    rating: "3.6/5 ★",
+    price: "₹9,499",
+    price2:9499,
+    offer: "UPTO 37% OFF",
   },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzg1XC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5 ",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzg0XC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzgzXC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzg2XC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzg1XC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzg0XC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzgzXC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzg2XC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzg1XC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzg0XC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzgzXC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzg2XC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzg1XC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzg0XC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
-  {
-    image:
-      "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMzMzgzXC9QaW5rX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
-    name: "Hello Kitty Moisture Absorbing Basketball",
-    rating: "4.0/5",
-    price: 1850,
-    discount: "UP TO 40% OFF",
-  },
+   
 ];
 // console.log(newarrival);
 display(newarrival);
@@ -134,7 +15,7 @@ function display(newarrival) {
   newarrival.forEach(function (elem) {
     let box = document.createElement("div");
     let img = document.createElement("img");
-    img.setAttribute("src", elem.image);
+    img.setAttribute("src", elem.img);
     let name = document.createElement("p");
     name.innerText = elem.name;
     let ratings = document.createElement("p");
@@ -143,9 +24,14 @@ function display(newarrival) {
     let price = document.createElement("h2");
     price.innerText = elem.price;
     let discount = document.createElement("h3");
-    discount.innerText = elem.discount;
+    discount.innerText = elem.offer;
     box.append(img, name, ratings);
     box2.append(price, discount);
+    img.addEventListener("click", function () {
+      console.log("inside the function")
+      localStorage.setItem("product", JSON.stringify(elem));
+      window.location.href = "pr.html";
+    });
     box3 = document.createElement("div");
     box3.append(box, box2);
     document.querySelector("#main").append(box3);
